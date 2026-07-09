b3a8490ba8fc1baaf9b61219be3bd0f8dc7fcfca
Hanhai <1932232849@qq.com>
2026-07-09 14:35:46 +0800

    Step 9: build_position_index_column

--- model.py
+++ model.py
@@ -66,7 +66,7 @@ def compute_positional_div_term(d_model: int):
 import torch
 
 
-def build_position_index_column(max_len):
+def build_position_index_column(max_len: int):
     """Return a (max_len, 1) float tensor of [0, 1, ..., max_len-1]."""
     return torch.arange(max_len, dtype=torch.float32)[:, None]
 
