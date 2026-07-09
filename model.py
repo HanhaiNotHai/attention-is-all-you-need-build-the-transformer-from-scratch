911e33c5878c6142d60cb51a0f235bd25b89ad81
Hanhai <1932232849@qq.com>
2026-07-09 11:59:38 +0800

    Step 2: build_id_to_token_vocab

--- model.py
+++ model.py
@@ -20,8 +20,9 @@ def build_token_to_id_vocab(
                 i += 1
     return vocab
 
-# Step 2 - build_id_to_token_vocab (not yet solved)
-# TODO: implement
+# Step 2 - build_id_to_token_vocab
+def build_id_to_token_vocab(token_to_id: dict[str:int]):
+    return {i: token for token, i in token_to_id.items()}
 
 # Step 3 - encode_sentence_to_ids (not yet solved)
 # TODO: implement
