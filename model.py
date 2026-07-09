fa71fe5232ecd544fabd92dced943063fd075fdf
Hanhai <1932232849@qq.com>
2026-07-09 15:16:21 +0800

    Step 4: decode_ids_to_tokens

--- model.py
+++ model.py
@@ -36,6 +36,8 @@ def encode_sentence_to_ids(sentence: str, token_to_id: dict[str, int], unk_token
 
 # Step 4 - decode_ids_to_tokens
 def decode_ids_to_tokens(ids: list[int], id_to_token: dict[int, str]):
+    '''map each id in ids to its token string via id_to_token and return the list'''
+
     return [id_to_token[i] for i in ids]
 
 # Step 5 - pad_id_sequence
